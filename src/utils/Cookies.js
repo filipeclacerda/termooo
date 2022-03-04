ab5dfb43c5c4dabed9113299e1f185141bcbb49f
Filipe Lacerda <filipeclacerda@gmail.com>
2022-03-04 10:53:51 -0300

    attempt SameSite=Lax for cookies

--- src/utils/Cookies.js
+++ src/utils/Cookies.js
@@ -3,7 +3,7 @@ export function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
   let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";SameSite=None;" + expires + ";path=/";
+  document.cookie = cname + "=" + cvalue + ";SameSite=Lax;" + expires + ";path=/";
 }
 
 export function getCookie(cname) {
